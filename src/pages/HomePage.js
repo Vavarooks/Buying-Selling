1ab6c074464371d8f5a00b065e39cbbae4682eb6
Vavarooks <vavarooks123@gmail.com>
2020-07-13 11:16:19 -0700

    alot of improvements

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,39 +1,17 @@
 import React from "react";
-import Buy from "./Buypage";
-import Seller from "./Sellerpage";
+import Body from "./Body";
+
 
 function Home() {
   return (
     <div>
-      <head>
-        <meta charset="utf-8"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-          <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous" />
-          <title>Hello, world!</title>
-  `   </head>
-        <body>
-          <h1>Hello, world!</h1>
-<ul className="nav nav-tabs" id="myTab" role="tablist">
-  <li className="nav-item" role="presentation">
-    <a className="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">Home</a>
-  </li>
-  <li className="nav-item" role="presentation">
-    <a className="nav-link" id="buy-tab" data-toggle="tab" href="#buy" role="tab" aria-controls="buy" aria-selected="false">Buy</a>
-  </li>
-  <li className="nav-item" role="presentation">
-    <a className="nav-link" id="sell-tab" data-toggle="tab" href="#sell" role="tab" aria-controls="sell" aria-selected="false">Sell</a>
-  </li>
-</ul>
-<div className="tab-content" id="myTabContent">
-  <div className="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab"><Home/></div>
-  <div className="tab-pane fade" id="buy" role="tabpanel" aria-labelledby="buy-tab"><Buy/></div>
-  <div className="tab-pane fade" id="sell" role="tabpanel" aria-labelledby="sell-tab"><Seller/></div>
-</div>
-
-          <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-          <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-          <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-        </body>
+        <h1>
+                Evelny's Art Gallary
+                </h1>
+            <div className="alert alert-success" role="alert">
+                Buy the art here! See all the prices!
+            </div>
+            <Body></Body>
     </div>
   );
 }
