862d8a165792bef20ffc56682c32585002efcd4b
Vavarooks <vavarooks123@gmail.com>
2020-07-13 10:56:44 -0700

    more edits

--- src/pages/Sellerpage.js
+++ src/pages/Sellerpage.js
@@ -4,7 +4,15 @@ import React from "react";
 function Seller() {
     return (
         <div>
-           
+            <div class="jumbotron">
+            <label for="basic-url">Your Image URL</label>
+            <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <span className="input-group-text" id="basic-addon3">https://example.com/users/</span>
+                </div>
+                <input type="text" className="form-control" id="basic-url" aria-describedby="basic-addon3" />
+            </div>
+            </div>
         </div>
     );
 }
